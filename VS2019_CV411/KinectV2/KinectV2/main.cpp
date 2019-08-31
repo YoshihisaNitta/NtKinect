b7bb18a9973865af51a8bf6483ab8dbd2a48e8f5
Yoshihisa Nitta <github@nc.tsuda.ac.jp>
2019-08-31 22:03:59 +0900

    Add files via upload

---
+++ VS2019_CV411/KinectV2/KinectV2/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016-2019 Yoshihisa Nitta
+ * Released under the MIT license
+ * http://opensource.org/licenses/mit-license.php
+ */
+/* http://nw.tsuda.ac.jp/lec/kinect2/ */
+
+#include <iostream>
+#include <sstream>
+
+#include "NtKinect.h"
+
+using namespace std;
+
+void doJob() {
+  NtKinect kinect;
+  while (1) {
+    kinect.setRGB();
+    cv::imshow("rgb", kinect.rgbImage);
+    auto key = cv::waitKey(1);
+    if (key == 'q') break;
+  }
+  cv::destroyAllWindows();
+}
+
+int main(int argc, char** argv) {
+  try {
+    doJob();
+  }
+  catch (exception& ex) {
+    cout << ex.what() << endl;
+    string s;
+    cin >> s;
+  }
+  return 0;
+}
